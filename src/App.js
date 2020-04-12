0f6a5bb1125f053e04d5827e32068a3cfb9700c3
lucas.fonseca <lucas.fonseca@trizy.com.br>
2020-04-11 23:53:28 -0300

    Fix: Atualizado FlatList

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import api from './services/api';
 
 import {
@@ -12,11 +13,11 @@ import {
 } from "react-native";
 
 export default function App() {
-  const [repositories, setRepositories] = useState([]);
+  const [repo, setRepo] = useState([]);
 
   useEffect(() => {
     api.get('repositories').then(response => {
-      setRepositories(response.data);
+      setRepo(response.data);
     })
   }, []);
 
@@ -24,56 +25,60 @@ export default function App() {
     const response = await api.post(`repositories/${id}/like`);
 
     const likedRepository = response.data;
-
-    const repositoriesUpdated = repositories.map(repository => {
+    // console.log(likedRepository);
+    const repositoriesUpdated = repo.map(repository => {
       if (repository.id === id) {
         return likedRepository;
       } else {
-        return repository;
+        return reposit.ory;
       }
-    });
+    })
 
-    setRepositories(repositoriesUpdated);
+    setRepo(repositoriesUpdated);
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <FlatList
-          data={repositories}
-          keyExtractor={repository => repository.id}
-          renderItem={({ item: repository }) => (
-            <View style={styles.repositoryContainer}>
-              <Text style={styles.repository}>{repository.title}</Text>
+        <View style={styles.repositoryContainer}>
+          <FlatList
+            data={repo}
+            keyExtractor={repositories => repositories.id}
+            renderItem={({ item }) => (
+              <>
+                <Text style={styles.repository}>{item.title}</Text>
 
-              <View style={styles.techsContainer}>
-                {repository.techs.map(tech => (
-                  <Text key={tech} style={styles.tech}>
-                    {tech}
+                <View style={styles.techsContainer}>
+                  {item.techs.map(tech => (
+                    <Text key={tech} style={styles.tech}>
+                      {tech}
+                    </Text>
+                  ))}
+                </View>
+
+                <View style={styles.likesContainer}>
+                  <Text
+                    style={styles.likeText}
+                    // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                    testID={`repository-likes-${item.id}`}
+                  >
+                    {item.likes} curtida{item.likes > 1 ? 's' : ''}
                   </Text>
-                ))}
-              </View>
+                </View>
 
-              <View style={styles.likesContainer}>
-                <Text
-                  style={styles.likeText}
-                  testID={`repository-likes-${repository.id}`}
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleLikeRepository(item.id)}
+                  // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                  testID={`like-button-${item.id}`}
                 >
-                  {repository.likes} curtida{repository.likes > 1 ? 's' : ''}
-                </Text>
-              </View>
-
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => handleLikeRepository(repository.id)}
-                testID={`like-button-${repository.id}`}
-              >
-                <Text style={styles.buttonText}>Curtir</Text>
-              </TouchableOpacity>
-            </View>
-          )}
-        />
+                  <Text style={styles.buttonText}>Curtir</Text>
+                </TouchableOpacity>
+              </>
+            )}
+          />
+        </View>
       </SafeAreaView>
     </>
   );
